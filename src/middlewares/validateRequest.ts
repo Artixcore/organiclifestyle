612c28913065ee0ad68f1da7cb37b44880c11e41
iibrahim70 <aviknuhash70@gmail.com>
2024-03-20 13:02:08 +0600

    added apperror

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,17 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import { catchAsync } from '../utils/catchAsync';
 
 export const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation check
-      await schema.parseAsync({
-        body: req.body,
-      });
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // validation check
+    await schema.parseAsync({
+      body: req.body,
+    });
 
-      next();
-    } catch (error) {
-      next(error);
-    }
-  };
+    next();
+  });
 };
